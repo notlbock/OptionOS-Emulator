3820f26994517e0b7c31234dd4d3c61c691d236f
E <uyenla1224@gmail.com>
2026-01-07 19:40:29 -0800

    Add topbar drawing functionality

--- option/headers/topbar.h
+++ option/headers/topbar.h
@@ -4,7 +4,10 @@
 #include <raylib.h>
 
 inline void CreateTopbar(){
-  
+  Rectangle topbarRec = {0, 0, 800, 30};
+  Color topbarColor = {255, 255, 0, 255};
+
+  DrawRectangleRec(topbarRec, topbarColor);
 }
 
 #endif
